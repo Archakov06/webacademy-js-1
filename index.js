b1290855b53c6586644572a0f1c90d2811a2358c
Archakov06 <hello@archakov.im>
2017-06-03 12:36:17 +0300

    Внес поправки

--- index.js
+++ index.js
@@ -43,6 +43,28 @@ function removeItem(i){
   }
 }
 
+function saveItem(index){
+
+  var count = people.length;
+
+  for (var i = 0; i < count; i++) {
+    if ( i == index ) {
+
+      var first_name = $('#item-'+ index + ' input[name="first_name"]').val();
+      var last_name = $('#item-'+ index + ' input[name="last_name"]').val();
+      var email = $('#item-'+ index + ' input[name="email"]').val();
+      var phone = $('#item-'+ index + ' input[name="phone"]').val();
+
+      people[index].first_name = first_name;
+      people[index].last_name = last_name;
+      people[index].email = email;
+      people[index].phone = phone;
+
+    }
+  }
+
+}
+
 function editItem(i){
   var text = $('#edit-' + i).text(); // Редактировать
   var editable = text == 'Редактировать' ? true : false;
@@ -52,44 +74,48 @@ function editItem(i){
     if (editable) $(this).removeAttr('readonly'); else $(this).attr('readonly', '');
   });
 
+  if (!editable) {
+    saveItem(i);
+  }
+
   $('#edit-' + i).text(text);
 }
 
+function createItem(man, i){
+
+  var first_name = man.first_name;
+  var last_name = man.last_name;
+  var email = man.email;
+  var phone = man.phone;
+
+  var arr = [
+    '<tr id="item-'+ i +'">',
+    '  <td><input name="first_name" readonly value="'+ first_name +'"></td>',
+    '  <td><input name="last_name" readonly value="'+ last_name +'"></td>',
+    '  <td><input name="email" readonly value="'+ email +'"></td>',
+    '  <td><input name="phone" readonly value="'+ phone +'"></td>',
+    '  <td>',
+    '    <button id="remove-'+ i +'" onClick="removeItem('+ i +')" class="ui inverted red button">Удалить</button>',
+    '    <button id="edit-'+ i +'" onClick="editItem('+ i +')" class="ui inverted blue button">Редактировать</button>',
+    '  </td>',
+    '</tr>'
+  ];
+
+  var html = arr.join('');
+
+  $('tbody').append(html);
+
+}
+
+function createTable(){
+  var count = people.length;
+  for (var i = 0; i < count; i++) {
+    var man = people[i];
+    createItem(man, i);
+  }
+}
+
 $(document).ready(function(){
-
-	function createItem(man, i){
-
-    var first_name = man.first_name;
-    var last_name = man.last_name;
-    var email = man.email;
-    var phone = man.phone;
-
-    var arr = [
-      '<tr id="item-'+ i +'">',
-      '  <td><input readonly value="'+ first_name +'"></td>',
-      '  <td><input readonly value="'+ last_name +'"></td>',
-      '  <td><input readonly value="'+ email +'"></td>',
-      '  <td><input readonly value="'+ phone +'"></td>',
-      '  <td>',
-      '    <button id="remove-'+ i +'" onClick="removeItem('+ i +')" class="ui inverted red button">Удалить</button>',
-      '    <button id="edit-'+ i +'" onClick="editItem('+ i +')" class="ui inverted blue button">Редактировать</button>',
-      '  </td>',
-      '</tr>'
-    ];
-
-    var html = arr.join('');
-
-		$('tbody').append(html);
-
-	}
-
-	function createTable(){
-		var count = people.length;
-		for (var i = 0; i < count; i++) {
-			var man = people[i];
-			createItem(man, i);
-		}
-	}
 
 	createTable();
 
